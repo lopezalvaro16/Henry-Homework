b79d4b2b897ffc9687c6eb12e5a2aa2a3c583264
lopezalvaro16 <alvaro154020747@gmail.com>
2023-08-01 09:04:05 -0300

    Terminé el IntroToCs, ejercicio 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,29 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let sum = 0;
 
-function DecimalABinario(num) {}
+   num = num.split("").reverse()
+   for (let i = 0; i < num.length; i++) {
+      sum += (2**i)*num[i]
+   }
+   return sum
+}
+
+function DecimalABinario(num) {
+   if (num === 0) {
+     return '0';
+   }
+ 
+   let binario = '';
+   while (num > 0) {
+     const digitoBinario = num % 2;
+     binario = digitoBinario + binario;
+     num = Math.floor(num / 2);
+   }
+ 
+   return binario;
+ }
 
 module.exports = {
    BinarioADecimal,

34fb78471bc379ffc7e0bc902f467831c2a9d01d
lopezalvaro16 <alvaro154020747@gmail.com>
2023-08-09 09:02:04 -0300

    Terminé el Algoritmos |, ejercicio 7

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let matriz = [];
+  let primo = 2;
+  while (num > 1) {
+    while (num % primo === 0) {
+      matriz.push(primo);
+      num = num / primo;
+    }
+    primo++;
+  }
+  matriz.unshift(1);
+  return matriz;
 }
 
 function bubbleSort(array) {
@@ -14,27 +24,51 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  const n = array.length;
 
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+
+  return array;
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 1; i < array.length; i++) {
+    const currentElement = array[i];
+    let j = i - 1;
 
+    while (j >= 0 && array[j] > currentElement) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = currentElement;
+  }
+
+  return array;
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i; j > 0 && array[j] < array[j - 1]; j--) {
+      [array[j], array[j - 1]] = [array[j - 1], array[j]];
+    }
+  }
+  return array;
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------

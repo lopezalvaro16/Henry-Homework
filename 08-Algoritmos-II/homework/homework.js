8c62c54d2edbb42c03d184de750713ea60fb9edc
lopezalvaro16 <alvaro154020747@gmail.com>
2023-08-10 12:59:26 -0300

    Terminé el Algoritmos ||, ejercicio 8

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -6,7 +6,23 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array; // El array ya está ordenado
+  }
 
+  const pivot = array[0]; // Elegir el primer elemento como pivote
+  const izq = [];
+  const der = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      izq.push(array[i]);
+    } else {
+      der.push(array[i]);
+    }
+  }
+
+  return [...quickSort(izq), pivot, ...quickSort(der)];
 }
 
 function mergeSort(array) {
@@ -14,8 +30,43 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array; // El array ya está ordenado (caso base)
+  }
 
-  //okey probemos ahora
+  const mitad = Math.floor(array.length / 2);
+  const izq = array.slice(0, mitad);
+  const der = array.slice(mitad);
+
+  return merge(mergeSort(izq), mergeSort(der));
+}
+
+function merge(izq, der) {
+  let resultado = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < izq.length && j < der.length) {
+    if (izq[i] < der[j]) {
+      resultado.push(izq[i]);
+      i++;
+    } else {
+      resultado.push(der[j]);
+      j++;
+    }
+  }
+
+  while (i < izq.length) {
+    resultado.push(izq[i]);
+    i++;
+  }
+
+  while (j < der.length) {
+    resultado.push(der[j]);
+    j++;
+  }
+
+  return resultado;
 }
 
 // No modificar nada debajo de esta línea
